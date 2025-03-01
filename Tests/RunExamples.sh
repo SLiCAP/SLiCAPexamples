c6a6a8a0fb35c4f66a462e29ebba2a7ba9ef8e4c
Charles White <charles.white988@gmail.com>
2025-02-28 18:53:33 -1000

    This will run the tests and upload the html to github pages.

--- Tests/RunExamples.sh
+++ Tests/RunExamples.sh
@@ -2,13 +2,13 @@
 declare -A test_cirs
 # test_cirs is a dictionary with the directory as a key and the value as the run file
 test_cirs["balancedCircuits"]="balancedCircuitsProject.py"
-#test_cirs["CSstage"]="CSresnoise.py"
+##test_cirs["CSstage"]="CSresnoise.py"
 test_cirs["FilterDesign"]="LR4project.py"
 test_cirs["myFirstRCnetwork"]="myFirstRCnetwork.py"
 test_cirs["noiseFigure"]="noiseFigures.py"
-#This will make sure no plots are shown:
 mkdir allhtml
-cp Template.rst ExampleCirs.rst
+cp Template.html index.html
+#This will make sure no plots are shown:
 export MPLBACKEND=svg #For plotting with Matplotlib on the 'SVG' backend
 for key in "${!test_cirs[@]}"; do
     cd $key
@@ -16,5 +16,7 @@ for key in "${!test_cirs[@]}"; do
     mkdir ../allhtml/$key
     mv  -v html/* ../allhtml/$key
     cd ..
-    echo "   - \`${key} <../examples/${key}>\`_" >> ExampleCirs.rst
+    echo " <p><a href=\"./${key}/index.html\"> Link To ${key} Test</a></p>" >> index.html
 done
+echo "</body></html>" >> index.html
+mv -v index.html allhtml/index.html
